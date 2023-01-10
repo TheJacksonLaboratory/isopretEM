5dd965a50ed0fcfdf4e59a0f2be000c9839aed37
karleg <32402869+karleg@users.noreply.github.com>
2023-01-10 10:08:04 -0500

    paths to top of script

--- scripts/translate_isoforms.R
+++ scripts/translate_isoforms.R
@@ -1,35 +1,29 @@
 library(seqinr)
+
 library(Biostrings)
+
 library(data.table)
 
-###########################################################################
-# 1) Read the GTF and FASTA file, which can be obtained from Ensembl at
-# http://useast.ensembl.org/info/data/ftp/index.html/
-###########################################################################
 
-# Adjust the following path as necessary
-gtf.file.path = 'Homo_sapiens.GRCh38.91.gtf'
-gtf.file=fread(gtf.file.path, sep='\t', quote = '', data.table=FALSE)
+#Change the following paths as needed:
 
-# Adjust the following path as necessary
-# This file is used to extract the sequences accoridng to the GTF file
-# using the gffread tool in order for it to generate the protein sequence:
+path.to.gtf='/Users/karleg/STAR/STAR/bin/MacOSX_x86_64/data/GRCh38/annotation/Homo_sapiens.GRCh38.91.gtf'
 
-fasta.file='GRCh38_r91.all.fa'
+fasta.file='/Users/karleg/STAR/STAR/bin/MacOSX_x86_64/data/GRCh38/sequence/GRCh38_r91.all.fa'
 
-###########################################################################
-# 2) Extract the exon rows from the GTF file
-###########################################################################
+path.to.gffread='gffread'
+
+#In order to use the gffread tool for translating isoforms we need to read the GTF file:
+
+gtf.file=fread(path.to.gtf,sep='\t',quote = '',data.table = FALSE)
+
 gtf.file=gtf.file[gtf.file$V3=='exon',]
 
-###########################################################################
-# 3) Extract the corresponding transcript IDs
-###########################################################################
 transcript.ids=gsub(';','',unlist(lapply(strsplit(as.character(gtf.file[,9]),split=' '),'[[',6)))
+
 transcript.ids=gsub("\"",'',transcript.ids)
 
-
-
+#The following path to the genomic sequence is passed to the gffread tool in order for it to generate the protein sequence:
 
 input=mclapply(unique(transcript.ids),function(isoform.id)
 {
@@ -37,7 +31,7 @@ input=mclapply(unique(transcript.ids),function(isoform.id)
   
   write.table(tr.gtf,paste0("transcript",isoform.id,".gtf"),sep='\t',col.names = FALSE,row.names = FALSE,quote = FALSE)
   
-  system(paste0('gffread -y isoform_seqs/translated_',isoform.id,'.fa -g ',fasta.file," transcript",isoform.id,".gtf"))
+  system(paste0(path.to.gffread,' -y isoform_seqs/translated_',isoform.id,'.fa -g ',fasta.file," transcript",isoform.id,".gtf"))
   
   system(paste0("rm transcript",isoform.id,".gtf"))
   
